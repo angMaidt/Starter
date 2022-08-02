faa9c16572eac6f09fca7aa26eb222bbd938d218
Angie Maidt <angiemaidt@gmail.com>
2022-08-02 17:46:31 -0600

    maybe there on create?

--- app/api/recipe_routes.py
+++ app/api/recipe_routes.py
@@ -19,50 +19,51 @@ def get_recipes():
 @recipe_routes.route('', methods=['POST'])
 def post_recipe():
 
-    form1 = IngredientForm
-    form2 = InstructionForm
-    form3 = RecipeForm
+    form1 = RecipeForm()
+    form2 = IngredientForm()
+    form3 = InstructionForm()
 
     form1['csrf_token'].data = request.cookies['csrf_token']
     if form1.validate_on_submit():
-        ingredient = Ingredient(
-            amount = form1.data['amount'],
-            food_stuff = form1.data['food_stuff'],
-            measurement_unit_id = form1.data['measurement_unit_id'],
-            recipe_id = form1.data['recipe_id'],
+        recipe = Recipe(
+            user_id = form1.data['user_id'],
+            title = form1.data['title'],
+            image_url = form1.data['image_url'],
+            description = form1.data['description'],
+            prep_time = form1.data['prep_time'],
+            bake_time = form1.data['bake_time'],
+            active_time = form1.data['active_time'],
+            baking_temp = form1.data['baking_temp'],
+            total_yield = form1.data['total_yield']
         )
 
-        db.session.add(ingredient)
+        db.session.add(recipe)
         db.session.commit()
 
-        form3['csrf_token'].data = request.cookies['csrf_token']
+        form2['csrf_token'].data = request.cookies['csrf_token']
         if form2.validate_on_submit():
-            instruction = Instruction(
-                list_order = form2.data['list_order'],
-                specification = form2.data['specification'],
-                recipe_id = form2.data['recipe_id']
+            ingredient = Ingredient(
+                amount = form2.data['amount'],
+                food_stuff = form2.data['food_stuff'],
+                measurement_unit_id = form2.data['measurement_unit_id'],
+                recipe_id = form2.data['recipe_id'],
             )
 
-            db.session.add(instruction)
-            db.session.commit()
+            db.session.add(ingredient)
+            # db.session.commit()
 
-            form1['csrf_token'].data = request.cookies['csrf_token']
+            form3['csrf_token'].data = request.cookies['csrf_token']
             if form3.validate_on_submit():
-                recipe = Recipe(
-                    user_id = form3.data['user_id'],
-                    title = form3.data['title'],
-                    image_url = form3.data['image_url'],
-                    description = form3.data['description'],
-                    prep_time = form3.data['prep_time'],
-                    bake_time = form3.data['bake_time'],
-                    active_time = form3.data['active_time'],
-                    baking_temp = form3.data['baking_temp'],
-                    total_yield = form3.data['total_yield']
+                instruction = Instruction(
+                    list_order = form3.data['list_order'],
+                    specification = form3.data['specification'],
+                    recipe_id = form3.data['recipe_id']
                 )
 
-                db.session.add(recipe)
+                db.session.add(instruction)
                 db.session.commit()
                 return recipe.to_dict()
+                
     return {'errors': [validation_errors_to_error_messages(form1.errors), validation_errors_to_error_messages(form2.errors), validation_errors_to_error_messages(form3.errors)]}, 401
 
 
