c3714b994829c8707429d8bc6d166ea07311021c
Angie Maidt <angiemaidt@gmail.com>
2022-08-05 17:05:48 -0600

    recipe validators rendering corectly

--- react-app/src/components/Recipes/RecipeForms/NewRecipeForm/NewRecipeForm.js
+++ react-app/src/components/Recipes/RecipeForms/NewRecipeForm/NewRecipeForm.js
@@ -51,30 +51,42 @@ function NewRecipeForm() {
         fetchRecipes().catch(console.error)
     }, [dispatch])
 
+    const filetypes_regex = /\.(gif|jpe?g|png|webp)$/
+
     //validations
-    useEffect =(() => {
+    useEffect(() => {
         let errors = []
+
         if (!title.length || title.length < 5) errors.push('Uh oh, your title is too short! Make it over 5 characters.')
         if (title.length > 50) errors.push('Uh oh, your title is too long! Make it less than 50 characters.')
+
         if (!description || description.length < 5) errors.push('Uh oh, your description is too short! Make it over 5 characters.')
         if (description.length > 2000) errors.push('Uh oh, your description is too long! Make it less than 2000 characters.')
-        if (!image_url) errors.push('Please enter an image file for your recipe.')
-        if ((/\.(gif|jpe?g|png|webp)$/i).test(image_url)) errors.push('Please enter a filename ending in .jpg, .jpeg, .png, .gif, or .webp.')
 
+        if (!image_url) errors.push('Please enter an image file for your recipe.')
+        if (!(filetypes_regex).test(image_url)) errors.push('Please enter a filename ending in .jpg, .jpeg, .png, .gif, or .webp.')
+
+        if (isNaN(active_time_mins) || isNaN(active_time_hrs)) errors.push('Please enter numbers only into active time fields!')
         if (active_time_mins < 0 || active_time_hrs < 0) errors.push('Looks like you tried to enter a negative number for active time. While impressive, not very realistic.')
         if (active_time_mins > 59) errors.push('Looks like you tried to enter 60 or more in the active time minutes! Please use hour field instead.')
         if (active_time_hrs > 100) errors.push('Looks like you entered over 100 hours active time on this recipe. That seems a little excessive, no?.')
 
+        if (isNaN(ferment_time_mins) || isNaN(ferment_time_hrs)) errors.push('Please enter numbers only into ferment time fields!')
         if (ferment_time_mins < 0 || ferment_time_hrs < 0) errors.push('Looks like you tried to enter a negative number for ferment time. While impressive, not very realistic.')
         if (ferment_time_mins > 59) errors.push('Looks like you tried to enter 60 or more in the ferment time minutes! Please use hour field instead.')
         if (ferment_time_hrs > 500) errors.push('Looks like you entered over 500 hours ferment time on this recipe. That seems a little excessive, no?.')
 
+        if (isNaN(bake_time_mins) || isNaN(bake_time_hrs)) errors.push('Please enter numbers only into bake time fields!')
         if (bake_time_mins < 0 || bake_time_hrs < 0) errors.push('Looks like you tried to enter a negative number for bake time. While impressive, not very realistic.')
         if (bake_time_mins > 59) errors.push('Looks like you tried to enter 60 or more in the bake time minutes! Please use hour field instead.')
         if (bake_time_hrs > 500) errors.push('Looks like you entered over 500 hours bake time on this recipe. That seems a little excessive, no?.')
 
         if (!total_yield || total_yield.length < 3) errors.push('Total yield is too short! Make it over 3 characters.')
-        if (total_yield.length > 50) errors.push('Total yield is too long! Make it less than 50 characters')
+        if (total_yield.length > 50) errors.push('Total yield is too long! Make it less than 50 characters.')
+
+        if (!baking_temp || baking_temp < 0) errors.push('Please enter a number over 0 for your bake temperature!')
+        if (baking_temp > 1000) errors.push('Did you enter a number over 1000 for bake temperature? I am skeptical, to say the least.')
+
         setValidationErrors(errors)
     }, [title, description, image_url, total_yield, active_time_mins, active_time_hrs, ferment_time_mins, ferment_time_hrs, bake_time_mins, bake_time_hrs, baking_temp, ])
 
@@ -122,24 +134,25 @@ function NewRecipeForm() {
             total_yield
         }
 
-        setTitle('')
-        setDescription('')
-        setImage_url('')
-        setActive_time_mins('')
-        setActive_time_hrs('')
-        setFerment_time_mins('')
-        setFerment_time_hrs('')
-        setBake_time_mins('')
-        setBake_time_hrs('')
-        setBaking_temp('')
-        setBaking_temp_system('fahrenheit')
-        setTotal_yield('')
-
         try {
             const data = await dispatch(postRecipeThunk(payload))
-            alert('Recipe Submitted!')
-            setRecipe_id(data.id)
-            setHasCreated(true)
+            if (data) {
+                alert('Recipe Submitted!')
+                setRecipe_id(data.id)
+                setHasCreated(true)
+                setTitle('')
+                setDescription('')
+                setImage_url('')
+                setActive_time_mins('')
+                setActive_time_hrs('')
+                setFerment_time_mins('')
+                setFerment_time_hrs('')
+                setBake_time_mins('')
+                setBake_time_hrs('')
+                setBaking_temp('')
+                setBaking_temp_system('fahrenheit')
+                setTotal_yield('')
+            }
             // history.push('/recipes')
         } catch (e) {
             setValidationErrors(e.errors)
@@ -149,6 +162,13 @@ function NewRecipeForm() {
     return (
         <>
         <h3>Recipe Body</h3>
+        {hasSubmitted && validationErrors.length > 0 &&
+            <ul className='errors'>
+                {validationErrors.map(error => (
+                    <li className='error' key={error}>{error}</li>
+                ))}
+            </ul>
+        }
             <form className='recipe-form' onSubmit={handleSubmit}>
                 <div className='recipe-input-container'>
                     <div className="input-container">
@@ -175,7 +195,7 @@ function NewRecipeForm() {
                         <label>Image Link</label>
                         <input
                             type='text'
-                            placeholder='Link an image that ends in .jpg, .jpeg, .png, or .gif'
+                            placeholder='yummy-bread.jpg'
                             // required
                             value={image_url}
                             onChange={(e) => setImage_url(e.target.value)}
