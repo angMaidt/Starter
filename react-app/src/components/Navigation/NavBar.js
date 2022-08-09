1dad156a25565fe1ab66ec49ab03e18179e86e7b
Angie Maidt <angiemaidt@gmail.com>
2022-08-09 00:17:23 -0600

    final commit tonight for real

--- react-app/src/components/Navigation/NavBar.js
+++ react-app/src/components/Navigation/NavBar.js
@@ -45,9 +45,9 @@ const NavBar = () => {
                 <h3>Home</h3>
               </div>
             </NavLink>
-            <NavLink to='/crash-course' exact={true} activeClassName='active' activeStyle={{ 'textDecoration': 'underline' }} style={{ 'textDecoration': 'none' }}>
+            <NavLink to='/sourdough-faq' exact={true} activeClassName='active' activeStyle={{ 'textDecoration': 'underline' }} style={{ 'textDecoration': 'none' }}>
               <div className='tab'>
-                <h3>Crash Course</h3>
+                <h3>F.A.Q.</h3>
               </div>
             </NavLink>
             <NavLink to='/recipes' activeClassName='active' activeStyle={{ 'textDecoration': 'underline' }} style={{ 'textDecoration': 'none' }}>
