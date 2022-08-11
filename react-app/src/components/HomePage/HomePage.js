57066d0311366691b26eb753793cb292a0bca10d
Angie Maidt <angiemaidt@gmail.com>
2022-08-11 12:28:35 -0600

    banner looking good

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -2,8 +2,13 @@ import './HomePage.css'
 
 function HomePage() {
     return (
-        <>
-            {/* <div className='welcome-ribbon'></div> */}
+        <div className='homepage'>
+            <div className='welcome-ribbon'>
+                <div className='welcome-ribbon-text'>
+                    <h3><span>Explore sourdough recipes.</span> <span>Share your own.</span> <span>Leave and recieve feedback.</span></h3>
+                    <h3>Founded in 2022, serving 7 million bread nerds a month.</h3>
+                </div>
+            </div>
             <div className="banner">
                 <div className='banner-image-container'>
                     <img src='../../../static/lined-up.jpg' alt='banner-1' />
@@ -23,7 +28,7 @@ function HomePage() {
                 </div>
             </div>
             {/* <div className='welcome-ribbon'></div> */}
-        </>
+        </div>
     )
 }
 
